6d06690467d3dcb8ba2ee41add6f24b93cfb79f7
pradeep <pradeepkumar.sf123@gmail.com>
2023-02-09 17:10:59 +0530

    Create V1.1.3__insertmock2.sql.sql

---
+++ dbscripts/V1.1.3__insertmock2.sql.sql
@@ -0,0 +1,5 @@
+use schema DEMO1.PUBLIC;
+insert into MOCK_DATAAAA (id, first_name, last_name, email) values (3, 'Yale', 'Yarwood', 'yyarwood2@berkeley.edu');
+insert into MOCK_DAT (id, first_name, last_name, email) values (4, 'Sammy', 'Georgins', 'sgeorgins3@apache.org');
+insert into MOCK_DAT (id, first_name, last_name, email) values (5, 'Raine', 'Crowne', 'rcrowne4@craigslist.org');
+insert into MOCK_DAT (id, first_name, last_name, email) values (7, 'Red', 'Benion', 'rbenion5@shutterfly.com');
