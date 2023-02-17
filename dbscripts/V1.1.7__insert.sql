7ca1bee12709b6670b8caf90b0329e201c449481
Pradeep <124242501+Pradeepbathala@users.noreply.github.com>
2023-02-17 15:19:18 +0530

    Add files via upload

---
+++ dbscripts/V1.1.7__insert.sql
@@ -0,0 +1,6 @@
+use schema DEMO1.PUBLIC;
+insert into students (id, first_name, last_name, email) values (1, 'Nerta', 'Ivie', 'nivie0@globo.com');
+insert into students (id, first_name, last_name, email) values (2, 'Morry', 'Hancke', 'mhancke1@biblegateway.com');
+insert into students (id, first_name, last_name, email) values (3, 'Joycelin', 'Kippen', 'jkippen2@tuttocitta.it');
+insert into students (id, first_name, last_name, email) values (4, 'Valdemar', 'Theobalds', 'vtheobalds3@livejournal.com');
+insert into students (id, first_name, last_name, email) values (5, 'Shawn', 'O''Canavan', 'socanavan4@cdbaby.com');
